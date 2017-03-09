b023b835e40b572adf4fd6f697eba97ec0c8ac9f
Cliquar <postitherepls@aol.de>
2017-03-09 15:18:41 +0100

    set new root

--- demoblog/config/routes.rb
+++ demoblog/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :blog_entries
   get 'static_pages/index'
-  root 'static_pages#index'
+  root 'blog_entries#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
